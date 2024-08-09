6a09fd4ea2d8ddbc37df81a1d94d6e04b1d876dd
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-08-09 12:17:30 -0300

    Manejo de AppRoutes

---
+++ src/app/(AppRoutes)/acerca/page.tsx
@@ -0,0 +1,9 @@
+//Se crea la nuevs ruta localhost:3000/acerca
+
+export default function Acerca() {
+  return (
+    <>
+      <h1>Acerca</h1>
+    </>
+  )
+}
---
+++ src/app/clientComponent/page.tsx
@@ -0,0 +1,9 @@
+"use client" //Para definir un componente del lado del cliente
+
+export default function clientComponent() {
+  return (
+    <div>
+      <h1>Componente de Cliente</h1>
+    </div>
+  )
+}
