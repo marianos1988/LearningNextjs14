f8323dd358d53ae187713f3a2e84e8c33dac7d80
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-08-13 16:35:37 -0300

    tipos de titulos de metadata

--- src/app/(AppRoutes)/acerca/page.tsx
+++ src/app/(AppRoutes)/acerca/page.tsx
@@ -1,4 +1,16 @@
+import { Metadata } from "next";
+
 //Se crea la nuevs ruta localhost:3000/acerca
+
+
+
+// Genera Metadata inicial
+export const metadata: Metadata = {
+  title: {
+    absolute: "Acerca"
+  },
+  description: "Aprendiendo Nextjs",
+};
 
 export default function Acerca() {
   return (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,20 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 // Genera Metadata inicial
-export const metadata: Metadata = {
-  title: "Curso Nextjs",
-  description: "Aprendiendo Nextjs",
-};
+// export const metadata: Metadata = {
+//   title: "Curso Nextjs",
+//   description: "Aprendiendo Nextjs",
+// };
+
+export const metadata:Metadata = {
+  title: {
+    template: "%s - Aprendiendo Next.js", // Complementa el titulo si hay otra metadata generada en el %s se carga el otro metadata
+    default: "Aprendiendo Nextjs", //valor por defecto
+    absolute: "", //Titulo es absoluto y no se complementa con el template
+  },
+}
+
+
 
 export default function RootLayout({
   children,
