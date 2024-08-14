c614580fc5cd98af994918ea3b6293753794f730
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-08-14 16:24:50 -0300

    Manejar Loading para los Fetch

---
+++ src/app/(AppRoutes)/(Dinamics AppRoutes)/productos/loading.tsx
@@ -0,0 +1,9 @@
+
+//Componente que sirve para mostrar cuando mandamos una peticion algun servidor y tarda en decargar
+export default function Loading() {
+  return (
+    <>
+      <h1>Cargando...</h1>
+    </>
+  )
+}
